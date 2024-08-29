2193d9cca2aba3d755f375f117027bd7a9756635
Your Name <your.email@example.com>
2024-08-29 10:48:31 +0300

    new1233

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://networking-final-u1h6.vercel.app/' // Ensure this matches your backend port
+  baseURL: 'https://networking-final.vercel.app/' // Ensure this matches your backend port
 });
 
 export default instance;
